012119b458ff0d1fbd2c755a454889c3c5dbb0d0
Keil-ENZO <enzo.keil06@icloud.com>
2023-12-04 15:14:42 +0100

    install tailwindcss

--- index.php
+++ index.php
@@ -37,10 +37,11 @@ try {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="./style.css">
+    <title>Portfolio</title>
 </head>
 
-<body>
+<body class="bg-slate-500">
     <h1>Portfolio</h1>
 
 </body>
